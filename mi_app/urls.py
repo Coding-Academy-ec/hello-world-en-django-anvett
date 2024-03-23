cfe699eb9d5990505e5786584ec7db5d12cc8526
Andres Veintimilla <andveti@gmail>
2024-03-22 19:37:16 -0500

    Practica paso test sin errores

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_world
+from .views import Hello_world
 
 urlpatterns = [
     # define la ruta para la vista hello_world.
+    path('hello/', Hello_world)
 ]
